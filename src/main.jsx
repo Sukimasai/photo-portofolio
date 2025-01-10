f29b69d63447b9fcdd6c01fc85bc2748a50eb23c
Sukimasai <justinreagan.12@gmail.com>
2025-01-10 20:48:08 +0700

    image

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import LinkContainer from './Components/LinkContainer';
 import BackgroundBlob from './Components/BackgroundBlob';
 
 const photoArrayOne = [{id: 1, link: "/images/01.jpg"},
-    {id: 2, link: "/images/02.jpg"},
-    {id: 3, link: "/images/03.jpg"},
-    {id: 4, link: "/images/04.jpg"},
-    {id: 5, link: "/images/05.jpg"},
-    {id: 6, link: "/images/06.jpg"},
+    {id: 2, link: "images/02.jpg"},
+    {id: 3, link: "images/03.jpg"},
+    {id: 4, link: "images/04.jpg"},
+    {id: 5, link: "images/05.jpg"},
+    {id: 6, link: "images/06.jpg"},
 ];
 
-const linkArrayOne = [{id: 1, link: "https://www.instagram.com/reaganjustn/", icon: "/images/instagram.png"}, 
-{id: 2, link: "https://github.com/Sukimasai", icon: "/images/github.png"},
-{id:3, link: "https://sukimasai.github.io/sunflower/", icon: "/images/flower.png"},
+const linkArrayOne = [{id: 1, link: "https://www.instagram.com/reaganjustn/", icon: "images/instagram.png"}, 
+{id: 2, link: "https://github.com/Sukimasai", icon: "images/github.png"},
+{id:3, link: "https://sukimasai.github.io/sunflower/", icon: "images/flower.png"},
 ]
 
 createRoot(document.getElementById('root')).render(
