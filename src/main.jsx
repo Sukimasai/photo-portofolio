86e1e68e5be9c91a96a0c9787e57c3471148a2cf
Sukimasai <justinreagan.12@gmail.com>
2025-01-10 20:57:33 +0700

    images

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import HeaderComponent from './Components/HeaderComponent';
 import LinkContainer from './Components/LinkContainer';
 import BackgroundBlob from './Components/BackgroundBlob';
 
-const photoArrayOne = [{id: 1, link: "/images/01.jpg"},
-    {id: 2, link: "images/02.jpg"},
-    {id: 3, link: "images/03.jpg"},
-    {id: 4, link: "images/04.jpg"},
-    {id: 5, link: "images/05.jpg"},
-    {id: 6, link: "images/06.jpg"},
+const photoArrayOne = [{id: 1, link: "/photo-portofolio/images/01.jpg"},
+    {id: 2, link: "/photo-portofolio/images/02.jpg"},
+    {id: 3, link: "/photo-portofolio/images/03.jpg"},
+    {id: 4, link: "/photo-portofolio/images/04.jpg"},
+    {id: 5, link: "/photo-portofolio/images/05.jpg"},
+    {id: 6, link: "/photo-portofolio/images/06.jpg"},
 ];
 
-const linkArrayOne = [{id: 1, link: "https://www.instagram.com/reaganjustn/", icon: "images/instagram.png"}, 
-{id: 2, link: "https://github.com/Sukimasai", icon: "images/github.png"},
-{id:3, link: "https://sukimasai.github.io/sunflower/", icon: "images/flower.png"},
+const linkArrayOne = [{id: 1, link: "https://www.instagram.com/reaganjustn/", icon: "/photo-portofolio/images/instagram.png"}, 
+{id: 2, link: "https://github.com/Sukimasai", icon: "/photo-portofolio/images/github.png"},
+{id:3, link: "https://sukimasai.github.io/sunflower/", icon: "/photo-portofolio/images/flower.png"},
 ]
 
 createRoot(document.getElementById('root')).render(
